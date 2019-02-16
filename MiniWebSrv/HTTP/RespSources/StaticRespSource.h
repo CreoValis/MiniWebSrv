6d9cb0263b13bf945a49a96f45b0b97da0813d46
Tallódi Mihály <msi1985@gmail.com>
2019-02-16 20:38:29 +0100

    StaticRespSource: documentation for the various ctors

--- MiniWebSrv/HTTP/RespSources/StaticRespSource.h
+++ MiniWebSrv/HTTP/RespSources/StaticRespSource.h
@@ -13,8 +13,13 @@ namespace RespSource
 class StaticRespSource : public IRespSource
 {
 public:
+	/**Creates a StaticRespSource by copying the specified string, and using the copy as the response.*/
 	StaticRespSource(std::string Response, const char *ContentType = "text/plain", const char *Charset = "utf-8", RESPONSECODE RespCode = RC_OK);
+	/**Creates a StaticRespSource by using the specified string's internal data as the response. The string must stay
+	constant and alive while any response object is alive.*/
 	StaticRespSource(const std::string *Response, const char *ContentType = "text/plain", const char *Charset = "utf-8", RESPONSECODE RespCode = RC_OK);
+	/**Creates a StaticRespSource by using the memory pointed to by the two pointers as the response. The string must
+	stay constant and alive while any response object is alive.*/
 	StaticRespSource(const char *ExtData, const char *ExtDataEnd, const char *ContentType = "text/plain", const char *Charset = "utf-8", RESPONSECODE RespCode = RC_OK);
 
 	virtual IResponse *Create(METHOD Method, const std::string &Resource, const QueryParams &Query, const std::vector<Header> &HeaderA,
